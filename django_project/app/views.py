6a5cff198a624edf7a760f7e0892ed50b12a2136
kamhome <feliks-roman@mail.ru>
2018-10-25 23:34:13 +0300

    add some css and pagination

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -1,38 +1,40 @@
 from django.views.generic import ListView, DetailView
 from app.models import Client, Department
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
+from app.helpers import get_records
+
+
+# class ClientList(ListView):
+#     context_object_name = 'clients_list'
+#     template_name = "home.html"
+#
+#     def get_queryset(self):
+#         clients = Client.objects.all()
+#         queryset = get_records(self.request, clients, 2)
+#         return queryset
 
 class ClientList(ListView):
     context_object_name = 'clients_list'
-    queryset = Client.objects.all()
     template_name = "home.html"
+    paginate_by = 2
 
-# class ClientDetail(ListView):
-#     context_object_name = 'client_data'
-#     template_name = 'client_detail.html'
-#
-#     def get_queryset(self):
-#
-#         # self.client = get_object_or_404(Client, id=self.kwargs['pk'])
-#         # raise Exception(self.client)
-#         return Client.objects.filter(id=self.kwargs['pk'])
-#
-# def get_accesses(request):
-#     list_groups = []
-#     for g in request.user.groups.all():
-#         list_groups.append(g.name)
-#     if 'managers' in list_groups:
-#         accesses = AccessRequest.objects.all()
-#     else:
-#         accesses = AccessRequest.objects.filter(name=request.user.username)
-#     return render(request, 'accesses/accesses.html', {'accesses' : accesses})
 
-class ClientDetail(DetailView):
-    model = Client
+    def get_queryset(self, *args, **kwargs):
+        if self.kwargs:
+            return Client.objects.filter(category=self.kwargs['category']).order_by('-createdAt')
+        else:
+            query = Client.objects.all().order_by('id')
+            return query
+
+
+
+
+
+class ClientDetail(ListView):
+    context_object_name = 'client_data'
     template_name = 'client_detail.html'
 
-    def get_context_data(self, **kwargs):
-        # self.client = get_object_or_404(Client, id=self.kwargs)
-        context = super(ClientDetail, self).get_context_data(**kwargs)
-        context['client_data'] = Client.objects.filter(username=kwargs)
-        return context
+    def get_queryset(self):
+        self.client = get_object_or_404(Client, id=self.kwargs['pk'])
+        return self.client
+
