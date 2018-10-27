276d66774fdf3858ec247db8034e3bd976716e14
kamhome <feliks-roman@mail.ru>
2018-10-27 15:18:12 +0300

    filters

--- django_project/app/filtersets.py
+++ django_project/app/filtersets.py
@@ -3,7 +3,29 @@ from app.models import Client
 
 
 class ClientFilter(django_filters.FilterSet):
+    VALUES_ORD =(
+        ('asc', 'ASC'),
+        ('desc', 'DESC')
+    )
+    ordering = django_filters.ChoiceFilter(label='Порядок', choices=VALUES_ORD, method='filter_by_order')
+
+    VALUES_WOR = (
+        ('true', 'Работают'),
+        ('false', 'Не работают')
+    )
+    now_work = django_filters.ChoiceFilter(label='Работает в компании', choices=VALUES_WOR, method='filter_by_end_work')
+
     class Meta:
         model = Client
-        fields = ['department', 'end_work_date']
+        fields = ['department',]
 
+    def filter_by_order(self, queryset, name, value):
+        expression = 'username' if value == 'asc' else '-username'
+        return queryset.order_by(expression)
+
+    def filter_by_end_work(self, queryset, name, value):
+        if value == 'true':
+           query = queryset.filter(end_work_date__isnull=True)
+        else:
+            query = queryset.filter(end_work_date__isnull=False)
+        return query
--- django_project/app/views.py
+++ django_project/app/views.py
@@ -3,7 +3,7 @@ from app.models import Client, Department
 from django.shortcuts import get_object_or_404, render
 from django_filters.views import FilterView
 from app.filtersets import ClientFilter
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # class ClientList(ListView):
 #     context_object_name = 'clients_list'
@@ -21,12 +21,37 @@ from app.filtersets import ClientFilter
 #         fields = ['department', 'end_work_date']
 
 class ClientList(FilterView):
-    context_object_name = 'clients_list'
+    # context_object_name = 'clients_list'
     template_name = "home.html"
-    paginate_by = 2
+    # paginate_by = 2
     model = Client
-    filter_class = ClientFilter
-    filterset_fields = {'department', 'username'}
+    # filter_class = ClientFilter
+    # filterset_fields = {'department', 'end_work_date'}
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientList, self).get_context_data()
+        context['filter'] = ClientFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+    # def get_queryset(self):
+    #     condo_list = self.model.objects.all()
+    #     condo_filter = ClientFilter(self.request.GET, queryset=condo_list)
+    #
+    #     paginator = Paginator(condo_filter.qs, 2)
+    #     page = self.request.GET.get('page')
+    #
+    #     try:
+    #         condos = paginator.page(page)
+    #     except PageNotAnInteger:
+    #         condos = paginator.page(1)
+    #     except EmptyPage:
+    #         condos = paginator.page(paginator.num_pages)
+    #
+    #     return {
+    #         'title': 'Home',
+    #         'condos': condos,
+    #         'page': page,
+    #         'condo_filter': condo_filter,
+    #     }
 
 
 class ClientDetail(ListView):
